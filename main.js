112d8c9978b4ac36ee5200a304bde97afa2c20ae
Davi Augusto <davi_augusto11@hotmail.com>
2022-05-11 17:48:22 -0300

    Conexão com o bd estabelecida e rotas adcionadas via router

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
 const express = require("express")
+const bodyParser = require("body-parser")
+const connection = require("./database/database")
+const categoriesControler = require("./categories/CategoriesController")
+const articleController = require("./article/ArticleController")
 const app = express()
+
 app.set('view engine', 'ejs')
-app.request(express.static('public'))
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+connection
+        .authenticate()
+        .then(()=>{
+            console.log("Conexão com o Banco de Dados feita com sucesso!!")
+        }).catch((error)=>{
+            console.log(error)
+        })
+
+
 
 app.get("/", (req, res)=>{
     res.render("index")
 })
 
-
+app.use("/", categoriesControler)
+app.use("/", articleController)
 
 
 
