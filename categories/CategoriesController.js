3bb0877f08a0ebbfeee4c8d972f916a155f0d0ef
Davi Augusto <davi_augusto11@hotmail.com>
2022-05-18 17:24:07 -0300

    Variável alterada na rota de salvar formulário e nova rota criada para deletar categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,49 @@
 const express = require("express")
 const router = express.Router()
+const Category = require("./Category")
+const slugify = require("slugify")
 
 router.get("/admin/categories/new", (req, res)=> {
     res.render("admin/categories/new")
 })
 
-router.get("/categories/save",(req, res) =>{
-    
+router.post("/categories/save",(req, res) =>{
+    let title = req.body.title
+    if(title != undefined){
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(()=>{
+            res.redirect("/")
+        })
+    }else{
+        res.redirect("/admin/categories/new")
+    }
+})
+
+router.get("/admin/categories", (req, res)=>{
+    Category.findAll().then(categories =>{
+        res.render("admin/categories/index", {categories: categories})
+    })
+})
+
+router.post("/categories/delete", (req, res) => {
+    var id = req.body.id
+    if(id != undefined){
+        if(!isNaN(id)){
+            Category.destroy({
+                where:{
+                    id: id
+                }
+            }).then(()=>{
+                res.redirect("/admin/categories")
+            })
+        }else{
+            res.redirect("admin/categories")
+        }
+    }else{
+        res.redirect("admin/categories")
+    }
 })
 
 module.exports = router;
