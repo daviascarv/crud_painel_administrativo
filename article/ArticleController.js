7420f06508909e16fdc1e0861318f9cedc35800d
Davi Augusto <davi_augusto11@hotmail.com>
2022-06-04 13:50:51 -0300

    Rota para receber os dados de artigos criados

--- article/ArticleController.js
+++ article/ArticleController.js
@@ -1,13 +1,32 @@
 const express = require("express")
 const router = express.Router()
+const Category = require("../categories/Category")
+const Article = require("./Article")
+const slugify = require("slugify")
 
-router.get("/articles", (req, res)=> {
-    res.send("Articles")
+router.get("/admin/article", (req, res)=> {
+    res.render("admin/article/index")
 })
 
-router.get("/admin/articles/new",(req, res) => {
-    res.send("Criar article")
+router.get("/admin/article/new",(req, res) => {
+    Category.findAll().then(categories =>{
+        res.render("admin/article/new", {categories: categories})
+    })
 })
 
+router.post("/article/save", (req, res)=>{
+    let title = req.body.title
+    let body = req.body.body
+    let category = req.body.category
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(()=>{
+        res.redirect("/admin/article")
+    })
+})
 
 module.exports = router;
