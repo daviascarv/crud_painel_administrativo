71334bd8f411befe9481f61952533444d79b70c0
Davi Augusto <davi_augusto11@hotmail.com>
2022-06-04 13:51:40 -0300

    Timezone do Brasil adcionado

--- database/database.js
+++ database/database.js
@@ -3,7 +3,8 @@ const Sequelize = require("sequelize")
 
 const connection = new Sequelize('guiapress', 'root', '@Vprdavi20',{
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: "-03:00"
 })
 
 module.exports = connection;
